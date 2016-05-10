2977d509c83985212caa12674938f8c1b77d9500
Michael Holler <michael.holler@reaktor.fi>
2016-05-10 19:07:20 +0300

    Adds Heia login without credentials

---
+++ Daily Scale/HeiaHandler.swift
@@ -0,0 +1,38 @@
+//
+//  HeiaHandler.swift
+//  Daily Scale
+//
+//  Created by Michael Holler on 10/05/16.
+//  Copyright © 2016 Holler. All rights reserved.
+//
+
+import Foundation
+
+class HeiaHandler {
+
+    func login(completion: (String) -> ()) {
+        var token:String?
+        let secret = Secret()
+        let params = "grant_type=password&username=\(secret.username)&password=\(secret.passwd)&client_id=\(secret.clientid)&client_secret=\(secret.secret)"
+        
+        let request = NSMutableURLRequest()
+        request.HTTPMethod = "POST"
+        request.HTTPBody = params.dataUsingEncoding(NSUTF8StringEncoding)
+        request.URL = NSURL(string: "https://api.heiaheia.com/oauth/token")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) in
+            do {
+                // TODO: crashes with no internets
+                if let jsonObject = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? [String:AnyObject] {
+                    NSOperationQueue.mainQueue().addOperationWithBlock {
+                        token = jsonObject["access_token"] as? String
+                        completion(token!)
+                    }
+                }
+            } catch {
+                print("Could not tokenize")
+            }
+        }
+        task.resume()
+    }
+}
---
+++ Daily Scale/Secrets.swift
@@ -0,0 +1,19 @@
+//
+//  Secrets.swift
+//  Daily Scale
+//
+//  Created by Michael Holler on 10/05/16.
+//  Copyright © 2016 Holler. All rights reserved.
+//
+
+import Foundation
+
+struct Secret {
+        let name = ""
+        let clientid = ""
+        let secret = ""
+        let redirect_uri = ""
+        let apiurl = ""
+        let username = ""
+        let passwd = ""
+}
